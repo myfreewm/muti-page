668998fab3eb8920000222508f51e7c78b8026eb
jorden <myfreewm@163.com>
2018-09-30 22:05:45 +0800

    finish mui page

---
+++ src/pages/main/Mains.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="main">
+  hello world
+ this is main dom
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
